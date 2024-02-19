207b1625b63287167a60e8f8683683d8edf1492a
mdg1317 <matthew.dennis137@gmail.com>
2024-02-19 01:42:43 -0800

    Added VARCHAR limits

--- EmployeeSQL/data_engineering.sql
+++ EmployeeSQL/data_engineering.sql
@@ -1,49 +1,49 @@
 -- Drop tables if they exist
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS titles;
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS dept_manager;
 DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS departments;
 
 -- Create tables
 CREATE TABLE departments (
-	dept_no VARCHAR PRIMARY KEY NOT NULL,
+	dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
 	dept_name VARCHAR
 );
 
 CREATE TABLE titles (
-	title_id VARCHAR PRIMARY KEY NOT NULL,
+	title_id VARCHAR(5) PRIMARY KEY NOT NULL,
 	title VARCHAR
 );
 
 CREATE TABLE employees (
 	emp_no INT PRIMARY KEY NOT NULL,
-	emp_title_id VARCHAR,
+	emp_title_id VARCHAR(5) NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
-	birth_date VARCHAR,
+	birth_date VARCHAR(10),
 	first_name VARCHAR,
 	last_name VARCHAR,
-	sex VARCHAR,
-	hire_date VARCHAR
+	sex VARCHAR(1),
+	hire_date VARCHAR(10)
 );
 
 CREATE TABLE dept_emp (
-	emp_no INT,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	dept_no VARCHAR,
+	dept_no VARCHAR(4) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR,
+	dept_no VARCHAR(4) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	emp_no INT,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries (
-	emp_no INT,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	salary INT
 );
